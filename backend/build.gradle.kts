c62daef3e62150e6225b1be73f9d2f194e32e055
CaptainCluster <sranta.ville@gmail.com>
2026-07-17 16:53:27 +0300

    init-backend - Backend project Spring Boot capabilities set

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
@@ -13,6 +15,12 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.test {
